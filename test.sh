9d3a17239168f53b103c3d4fec7eede912c1b729
larshp <larshp@hotmail.com>
2016-05-25 08:02:07 +0000

    some unit tests, close #4

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+./abapmerge test/abap/1/zmain.abap > /dev/null
+./abapmerge test/abap/2/zmain.abap > /dev/null
+./abapmerge test/abap/3/zmain.abap > /dev/null
+./abapmerge test/abap/4/zmain.abap > /dev/null
+echo "test.sh successful"
