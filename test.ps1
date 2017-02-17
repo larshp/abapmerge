365f7a7d2da4796dbaf85c181fef7046b580113c
Fabio Zuber <fabio.zuber@siemens.com>
2017-02-17 13:16:02 +0100

    chore: use forward slashes for paths, Idiot! :smile:

--- test.ps1
+++ test.ps1
@@ -1,7 +1,7 @@
-write-host "Test scripts"
-node abapmerge .\test\abap\1\zmain.abap > $null
-node abapmerge .\test\abap\2\zmain.abap > $null
-node abapmerge .\test\abap\3\zmain.abap > $null
-node abapmerge .\test\abap\4\zmain.abap > $null
-node abapmerge .\test\abap\5\zmain.abap > $null
-write-host "Tests finished" -ForegroundColor Green
+write-host "starting tests..."
+node abapmerge ./test/abap/1/zmain.abap > $null
+node abapmerge ./test/abap/2/zmain.abap > $null
+node abapmerge ./test/abap/3/zmain.abap > $null
+node abapmerge ./test/abap/4/zmain.abap > $null
+node abapmerge ./test/abap/5/zmain.abap > $null
+write-host "All Tests completed" -ForegroundColor Green
