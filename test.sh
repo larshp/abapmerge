6e354738ca461532d25175a3aa02755c761fc12a
Fabio Zuber <fabio.zuber@siemens.com>
2017-02-17 10:40:07 +0100

    chore: remove test batch and shell scripts

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-./abapmerge test/abap/1/zmain.abap > /dev/null
-./abapmerge test/abap/2/zmain.abap > /dev/null
-./abapmerge test/abap/3/zmain.abap > /dev/null
-./abapmerge test/abap/4/zmain.abap > /dev/null
-./abapmerge test/abap/5/zmain.abap > /dev/null
-echo "test.sh successful"
