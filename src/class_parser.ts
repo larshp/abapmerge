a8dd1dc4cafcc6ba215171af156c990ac7187246
Jakub Filak <jakub.filak@sap.com>
2018-07-01 00:59:33 +0200

    class: merge local definition & implementation
    I create classes with local definitions and implementations which helps
    me to develop unit tests much easier because I can easily mock behaviour
    of private implementation of public classes.
    
    In some of our systems, I am not allowed to create new packages and thus
    I cannot use abapGit deploy my development tools there. However, I can
    create customer space reports, so I needed to teach abapmerge to compile
    my ABAP tools.
    
    Local definitions are placed before public definition and local
    implementations are placed before public implementation. In both cases,
    deferred type declarations are placed in the beginning of the
    corresponding section.
    
    To avoid name collisions, local type names are converted to random
    strings of 30 characters where:
    - 0-4 : type hash (interface or class)
    - 5-19 : public class name hash
    - 20-29 : local type name hash
    
    Hashes are created as random strings using pseudo random generator where
    its seeds is computed from the name using an algorithm found somewhere in
    the Internet [1]. Also the PRG algorithm should mimic LCG with MINSTD
    parameters [2].
    
    This approach generating anonymous names should make sure the generated
    names stays unchanged between too runs.
    
    Unfortunately, I am not sure if the algorithms are strong enough to not
    produce any collision. If a collision happens, it will be discovered
    in the production because Activation is not going to work.
    
    If we run into problems with collisions we can consider different
    tactics:
    - use sequential numbers of public / local types
    - employ strong cryptographic algorithms
    
    At the moment, it is not trivial to detect collisions because:
    - anonymous types are generated when processing a class
    - anonymous types are not known after the class processing
    
    ----
    v2:
    
    @larshp pointed out in the review #53 that we should include mapping to
    the original name and the parent type to simplify problem analysis.
    
    I implemented his idea by prepending a comment with the old name and the
    public class name to declaration statement of the anonymized type.
    
    Example:
    * the original name lcl_def of the public class cl_foo anonymized to
      GiiGhXOXPMwTAnXjaE
    
    ```diff
      - class lcl_def definition.
      + * renamed: cl_foo :: lcl_def
      + class GiiGhXOXPMwTAnXjaE definition.
    ```
    
    ----
    v3:
    
    My initial idea was to make sure the renaming will affect only correct
    statements by doing simple syntax analysis. I remembered only class
    definition, interface definition and use of a type.
    
    However, there are many more syntax constructions where a type can
    appear:
    
    * exception in catch
    * exception in raising
    * class in inheriting from
    * class in friends
    * class in create object with type
    * interface in interfaces
    * interface in aliases
    * interface in methods
    * interface in method
    * interface in instances access
    
    and I am sure I forgot something.
    
    I found out it is no so easy to parse context grammar by regular
    expressions so I decided to simplify the algorithm to:
    * detect class definition to provide the old type name
    * detect interface definition to provide the old type name
    * rename all occurrences of the old type in non-comment text
    
    Beware that this brute force approach will rename the type occurrences
    in string literals.
    
    ----
    
    1: http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
    2: https://en.wikipedia.org/wiki/Lehmer_random_number_generator#Parameters_in_common_use

--- src/class_parser.ts
+++ src/class_parser.ts
@@ -1,25 +1,147 @@
 import File from "./file";
+import FileList from "./file_list";
 import Class from "./class";
+import Utils from "./utils";
 
-export default class ClassParser {
+export class AbapPublicClass {
+  public name: string;
+  public hash: string;
+  public def: string;
+  public imp: string;
+}
 
-  public static parse(f: File): Class {
+export class ClassParser {
+
+  public static anonymizeTypeName(prefix15: string, type: string, name: string): string {
+    let typeSeed = Utils.hashCodeOf(type);
+    let typeAlias = Utils.randomStringOfLength(typeSeed, 5);
+
+    let nameSeed = Utils.hashCodeOf(name);
+    let nameAlias = Utils.randomStringOfLength(nameSeed, 10);
+
+    return typeAlias + prefix15 + nameAlias;
+  }
+
+  public static renameLocalType(oldName: string, newName: string, parent: string, oldCode: string): string {
+    let occurrences = [
+      // interface declaration
+      { context: "* renamed: " + parent + " :: " + oldName + "\n",
+        regstr: "^(\s*interface\\s+)" + oldName + "(\\s*\\.)",
+      },
+      // class declaration
+      { context: "* renamed: " + parent + " :: " + oldName + "\n",
+        regstr: "^(\s*class\\s+)" + oldName + "(\\s+definition\\b)",
+      },
+      { context: "",
+        regstr:  "^([^*\"\\n]*\\b)" + oldName + "(\\b)",
+      },
+    ];
+
+    let newCode = oldCode;
+    for (let occurrence of occurrences) {
+      let regexp = new RegExp(occurrence.regstr, "igm");
+      newCode = newCode.replace(regexp, occurrence.context + "$1" + newName + "$2");
+    }
+
+    return newCode;
+  }
+
+  public static buildLocalFileName(part: string, publicClass: AbapPublicClass): string {
+    return publicClass.name + ".clas.locals_" + part + ".abap";
+  }
+
+  public static findFileByName(filename: string, list: FileList ): File {
+    filename = filename.toLowerCase();
+
+    let length = list.length();
+    for (let i = 0; i < length; ++i) {
+      let file = list.get(i);
+      if (filename === file.getFilename().toLowerCase()) {
+        return file;
+      }
+    }
+
+    return null;
+  }
+
+  public static parseLocalContents(part: string, publicClass: AbapPublicClass, local: string): void {
+    let global = local + "\n" + publicClass[part];
+
+    let regex = /^\s*((CLASS)\s+(\w+)\s+DEFINITION\s*[^\.]*|(INTERFACE)\s+(\w+)\s*)\./gim;
+    let definition;
+
+    /* tslint:disable:no-conditional-assignment */
+    while ((definition = regex.exec(local)) !== null) {
+      let type = definition[2];
+      let name = definition[3];
+
+      if (!type) {
+        // not class, dealing with interface
+        type = definition[4];
+        name = definition[5];
+      }
+
+      let alias = ClassParser.anonymizeTypeName(publicClass.hash, type, name);
+      global = ClassParser.renameLocalType(name, alias, publicClass.name, global);
+
+      // prepend (definition)? deferred
+      let decl = type + " " + alias;
+      if (type.toLowerCase() === "class") {
+        decl = decl + " DEFINITION";
+      }
+      global = decl + " DEFERRED.\n" + global;
+
+      // types of the local definitions are used in the (public|local)
+      // implementation. so, after we must update the implementation after
+      // we finish processing the local definitions.
+      if (part === "def") {
+        // this is the only difference between processing definitions and
+        // implementations - which can be handled by one function otherwise
+        publicClass.imp = ClassParser.renameLocalType(name, alias, publicClass.name, publicClass.imp);
+      }
+    }
+
+    publicClass[part] = global;
+  }
+
+  public static tryProcessLocalFile(part: string, publicClass: AbapPublicClass, list: FileList): void {
+    let filename = ClassParser.buildLocalFileName(part, publicClass);
+    let file = ClassParser.findFileByName(filename, list);
+    if (file === null) {
+      return;
+    }
+
+    let contents = file.getContents();
+    ClassParser.parseLocalContents(part, publicClass, contents);
+    file.markUsed();
+  }
+
+  public static parse(f: File, list: FileList): Class {
 
     let match = f.getContents().match(/^(([\s\S])*ENDCLASS\.)\s*(CLASS(.|\s)*)$/i);
     if (!match || !match[1] || !match[2] || !match[3]) {
       throw "error parsing class: " + f.getFilename();
     }
-    let name = f.getFilename().split(".")[0];
-    let def = this.makeLocal(name, match[1]);
 
-    let superMatch = def.match(/INHERITING FROM (Z\w+)/i);
+    let publicClass = new AbapPublicClass();
+    publicClass.name = f.getFilename().split(".")[0];
+    publicClass.hash = Utils.randomStringOfLength(Utils.hashCodeOf(publicClass.name), 15);
+    publicClass.def = this.makeLocal(publicClass.name, match[1]);
+    publicClass.imp = match[3];
+
+    // make sure we parse the imp part at the very first step!
+    ClassParser.tryProcessLocalFile("imp", publicClass, list);
+    // because the local definitions are used in the implementation.
+    ClassParser.tryProcessLocalFile("def", publicClass, list);
+
+    let superMatch = publicClass.def.match(/INHERITING FROM (Z\w+)/i);
 //    console.dir(superMatch);
     let dependencies = [];
     if (superMatch && superMatch[1]) {
       dependencies.push(superMatch[1].toLowerCase());
     }
 
-    return new Class(name, def, match[3], dependencies);
+    return new Class(publicClass.name, publicClass.def, publicClass.imp, dependencies);
   }
 
   private static makeLocal(name: string, s: string): string {
@@ -29,5 +151,4 @@ export default class ClassParser {
     ret = ret.replace(reg2, "FRIENDS ZCL_ABAPGIT");
     return ret;
   }
-
 }
---
+++ src/utils.ts
@@ -0,0 +1,43 @@
+export default class Utils {
+
+  public static minstdRandMax = 2147483646;
+
+  public static nextMinstdRandFor(seed: number): number {
+    let product = 48271 * seed;
+    return product % (Utils.minstdRandMax + 1);
+  }
+
+  public static hashCodeOf(input: string): number {
+    let hash = 0;
+
+    if (input.length === 0) {
+      return hash;
+    }
+
+    for (let i = 0; i < input.length; ++i) {
+      let char = input.charCodeAt(i);
+      /* tslint:disable:no-bitwise */
+      hash = ( ( hash << 5 ) - hash ) + char;
+      /* tslint:disable:no-bitwise */
+      hash = hash & hash;
+    }
+
+    if (hash < 0) {
+        return hash * -1;
+    }
+
+    return hash;
+  }
+
+  public static randomStringOfLength(seed: number, requiredLength: number): string {
+    let allowedLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    let result = "";
+    for (let currentLength = 0; currentLength < requiredLength; ++currentLength) {
+      seed = Utils.nextMinstdRandFor(seed);
+      result += allowedLetters[Math.floor(allowedLetters.length * (seed / Utils.minstdRandMax))];
+    }
+
+    return result;
+  }
+}
