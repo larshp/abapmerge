05565b52b7c3383c5422e9131e7596757575c784
Alexander Tsybulsky <sbcgua@users.noreply.github.com>
2021-08-01 16:49:19 +0300

    Better interface parser (#187)
    * better if parser
    
    * minor tweaks in Graph
    
    * minor tweak in ClassList
    
    * ignore reftos
    
    * better if parser
    
    * minor tweaks in Graph
    
    * minor tweak in ClassList
    
    * ignore reftos
    
    Co-authored-by: Lars Hvam <larshp@hotmail.com>

--- src/interface_parser.ts
+++ src/interface_parser.ts
@@ -5,35 +5,53 @@ export default class InterfaceParser {
 
   public static parse(f: File): Class {
 
-    let self = f.getFilename().split(".")[0];
-    let match = f.getContents().match(/^[\s\S]*(INTERFACE\s+\S+)\s+PUBLIC([\s\S]+)$/i);
-    if (!match || !match[1] || !match[2]) {
+    const self = f.getFilename().split(".")[0];
+    const ifDefinition = f.getContents().match(/^[\s\S]*(INTERFACE\s+\S+)\s+PUBLIC([\s\S]+)$/i);
+    if (!ifDefinition || !ifDefinition[1] || !ifDefinition[2]) {
       throw "error parsing interface: " + f.getFilename();
     }
 
-    let dependencies = [];
+    const dependencies = new Set();
 
-    let depMatch = f.getContents().match(/TYPE\s(ZIF_\w+)/ig);
-    if (depMatch) {
-      for (let dep of depMatch) {
-        let name = dep.substr(5).toLowerCase();
-        if (dependencies.indexOf(name) === -1 && name !== self) {
-          dependencies.push(name);
+    // this should capture just elements of an interface "zif=>..."
+    // if interface itself is referred, then it is probably a REF TO
+    // REF TOs will be solved by deferred definitions are not taken into account
+    // (maybe it is wrong by the way, as it helps to identify cyclic dependencies)
+    const typeDeps = f.getContents().matchAll(/TYPE\s+([^.,]+)?(ZIF_\w+)(=?)/ig);
+    if (typeDeps) {
+      for (const dep of typeDeps) {
+        const furtherIfElementMarker = dep[3];
+        if (!furtherIfElementMarker) {
+          const typeAtrrs = dep[1];
+          if (!/REF\s+TO/i.test(typeAtrrs)) {
+            throw new Error(`Unexpected interface ref: ${dep.toString()}`);
+          }
+          continue;
+        }
+        const ifName = dep[2].toLowerCase();
+        if (ifName !== self) {
+          dependencies.add(ifName);
         }
       }
     }
 
-    depMatch = f.getContents().match(/INTERFACES\s(ZIF_\w+)/ig);
-    if (depMatch) {
-      for (let dep of depMatch) {
-        let name = dep.substr(11).toLowerCase();
-        if (dependencies.indexOf(name) === -1 && name !== self) {
-          dependencies.push(name);
+    const interfaceDeps = f.getContents().matchAll(/INTERFACES(:)?\s+(ZIF_\w+)/ig);
+    if (interfaceDeps) {
+      for (const dep of interfaceDeps) {
+        const name = dep[2].toLowerCase();
+        if (name !== self) {
+          dependencies.add(name);
         }
       }
     }
 
-    return new Class(self, match[1] + match[2], false, "", dependencies);
+    return new Class(
+      self,
+      ifDefinition[1] + ifDefinition[2],
+      false,
+      "",
+      [...dependencies.values()] as string[]
+    );
 
   }
 
