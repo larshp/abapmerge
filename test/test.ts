c030a9149ba8dd77a41d97be5d15858f8e8451c3
Lars Hvam <larshp@hotmail.com>
2024-10-04 09:48:12 +0200

    enable testcase

--- test/test.ts
+++ test/test.ts
@@ -422,7 +422,7 @@ ENDFORM.`));
 });
 
 describe("test 27, chained includes", () => {
-  it.skip("something", () => {
+  it("something", () => {
     const files = new FileList();
     files.push(new File("zfoo.prog.abap",
       `REPORT zfoo.
