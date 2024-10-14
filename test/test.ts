339bb78709296c71d3d73c1d9fe654eb7d6c7c19
Mike Pokraka <mike@quirky.me.uk>
2024-10-14 15:42:23 +0100

    Revert whitespace

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-import { expect } from "chai";
+import {expect} from "chai";
 import Merge from "../src/merge";
 import File from "../src/file";
 import FileList from "../src/file_list";
@@ -16,9 +16,9 @@ describe("test 2, 2 includes", () => {
   it("something", () => {
     const files = new FileList();
     files.push(new File("zmain.abap", "report zmain.\n\n" +
-      "include zinc1.\n" +
-      "include zinc2.\n\n" +
-      "write / 'Main include'."));
+                        "include zinc1.\n" +
+                        "include zinc2.\n\n" +
+                        "write / 'Main include'."));
     files.push(new File("zinc1.abap", "write / 'hello @inc1'."));
     files.push(new File("zinc2.abap", "write / 'hello @inc2'."));
     expect(Merge.merge(files, "zmain")).to.be.a("string");
@@ -29,9 +29,9 @@ describe("test 3, subinclude", () => {
   it("something", () => {
     const files = new FileList();
     files.push(new File("zmain.abap", "report zmain.\n\n" +
-      "include zinc1.\n" +
-      "include zinc2.\n\n" +
-      "write / 'Main include'."));
+                        "include zinc1.\n" +
+                        "include zinc2.\n\n" +
+                        "write / 'Main include'."));
     files.push(new File("zinc1.abap", "include zsubinc1.\nwrite / 'hello @inc1'."));
     files.push(new File("zinc2.abap", "write / 'hello @inc2'."));
     files.push(new File("zsubinc1.abap", "write / 'hello @inc2'."));
@@ -43,9 +43,9 @@ describe("test 4, standard include", () => {
   it("something", () => {
     const files = new FileList();
     files.push(new File("zmain.abap", "report zmain.\n\n" +
-      "include zinc1.  \" A comment here\n" +
-      "include zinc2.\n\n" +
-      "write / 'Main include'."));
+                        "include zinc1.  \" A comment here\n" +
+                        "include zinc2.\n\n" +
+                        "write / 'Main include'."));
     files.push(new File("zinc1.abap", "include standard.\nwrite / 'hello @inc1'."));
     files.push(new File("zinc2.abap", "write / 'hello @inc2'."));
     expect(Merge.merge(files, "zmain")).to.be.a("string");
@@ -94,13 +94,13 @@ describe("test 9, @@abapmerge commands", () => {
   it("something", () => {
     const files = new FileList();
     files.push(new File("zmain.abap", "report zmain.\n" +
-      "\n" +
-      "write / 'Main include'.\n" +
-      "* @@abapmerge include style.css > write '$$'.\n" +
-      "  \" @@abapmerge include js/script.js > write '$$'.\n" +
-      "  \" @@abapmerge wrong pragma, just copy to output\n" +
-      "  \" @@abapmerge include data.txt > write '$$'.\n" +
-      "  \" @@abapmerge include data.txt > write '$$$'. \" Unescaped !"));
+                                      "\n" +
+                                      "write / 'Main include'.\n" +
+                                      "* @@abapmerge include style.css > write '$$'.\n" +
+                                      "  \" @@abapmerge include js/script.js > write '$$'.\n" +
+                                      "  \" @@abapmerge wrong pragma, just copy to output\n" +
+                                      "  \" @@abapmerge include data.txt > write '$$'.\n" +
+                                      "  \" @@abapmerge include data.txt > write '$$$'. \" Unescaped !"));
     files.push(new File("style.css", "body {\nbackground: red;\n}"));
     files.push(new File("data.txt", "content = 'X';\n"));
     files.push(new File("js/script.js", "alert(\"Hello world!\");\n"));
@@ -126,14 +126,14 @@ describe("test 11, simple class", () => {
     files.push(new File("zinc1.abap", "write / 'foo'."));
     files.push(
       new File("zcl_class.clas.abap",
-        "CLASS zcl_class DEFINITION PUBLIC CREATE PUBLIC.\n" +
-        "  PUBLIC SECTION.\n" +
-        "    CLASS-METHODS: blah.\n" +
-        "ENDCLASS.\n" +
-        "CLASS zcl_class IMPLEMENTATION.\n" +
-        "  METHOD blah.\n" +
-        "  ENDMETHOD.\n" +
-        "ENDCLASS.\n"));
+               "CLASS zcl_class DEFINITION PUBLIC CREATE PUBLIC.\n" +
+               "  PUBLIC SECTION.\n" +
+               "    CLASS-METHODS: blah.\n" +
+               "ENDCLASS.\n" +
+               "CLASS zcl_class IMPLEMENTATION.\n" +
+               "  METHOD blah.\n" +
+               "  ENDMETHOD.\n" +
+               "ENDCLASS.\n"));
     const result = Merge.merge(files, "zmain");
     expect(result).to.be.a("string");
     expect(result.split("\n").length).to.equal(17);
@@ -148,15 +148,15 @@ describe("test 12, @@abapmerge in class", () => {
     files.push(new File("style.css", "body {\nbackground: red;\n}"));
     files.push(
       new File("zcl_class.clas.abap",
-        "CLASS zcl_class DEFINITION PUBLIC CREATE PUBLIC.\n" +
-        "  PUBLIC SECTION.\n" +
-        "    CLASS-METHODS: blah.\n" +
-        "ENDCLASS.\n" +
-        "CLASS zcl_class IMPLEMENTATION.\n" +
-        "  METHOD blah.\n" +
-        "* @@abapmerge include style.css > write '$$'.\n" +
-        "  ENDMETHOD.\n" +
-        "ENDCLASS."));
+               "CLASS zcl_class DEFINITION PUBLIC CREATE PUBLIC.\n" +
+               "  PUBLIC SECTION.\n" +
+               "    CLASS-METHODS: blah.\n" +
+               "ENDCLASS.\n" +
+               "CLASS zcl_class IMPLEMENTATION.\n" +
+               "  METHOD blah.\n" +
+               "* @@abapmerge include style.css > write '$$'.\n" +
+               "  ENDMETHOD.\n" +
+               "ENDCLASS."));
 
     const result = Merge.merge(files, "zmain");
     expect(result).to.be.a("string");
@@ -170,7 +170,7 @@ describe("test 13, skip function groups", () => {
       new File("zmain.abap", "REPORT zmain."),
       new File("zabapgit_unit_te.fugr.saplzabapgit_unit_te.abap", "WRITE / 'Hello World!'."),
     ]);
-    const merged = Merge.merge(files, "zmain", { skipFUGR: true });
+    const merged = Merge.merge(files, "zmain", {skipFUGR: true});
     expect(merged).to.be.a("string");
     expect(merged).not.to.match(/Hello/);
   });
@@ -180,7 +180,7 @@ describe("test 13, skip function groups", () => {
       new File("zmain.abap", "REPORT zmain."),
       new File("zabapgit_unit_te.fugr.saplzabapgit_unit_te.abap", "WRITE / 'Hello World!'."),
     ]);
-    expect(() => Merge.merge(files, "zmain", { skipFUGR: false })).to.throw(/Not all files used.*fugr/);
+    expect(() => Merge.merge(files, "zmain", {skipFUGR: false})).to.throw(/Not all files used.*fugr/);
   });
 });
 
@@ -188,12 +188,12 @@ describe("test 14, include classes event without INCLUDE", () => {
   it("include classes event without INCLUDE", () => {
     const files = new FileList();
     files.push(new File("zmain13.prog.abap", "REPORT zmain13.\n" +
-      "\n" +
-      "write: 'Hello, world!'.\n"));
+                                             "\n" +
+                                             "write: 'Hello, world!'.\n"));
     files.push(new File("zcl_main.clas.abap", "class zcl_main defintion.\n" +
-      "endclass.\n" +
-      "class zcl_main implementation.\n" +
-      "endclass.\n"));
+                                              "endclass.\n" +
+                                              "class zcl_main implementation.\n" +
+                                              "endclass.\n"));
 
     const exp = "REPORT zmain13.\n" +
       "\n" +
@@ -215,13 +215,13 @@ describe("test 15, included classes are placed after whitespace and comments", (
   it("something", () => {
     const files = new FileList();
     files.push(new File("zmain13.prog.abap", "REPORT zmain13.\n" +
-      "* Comment asterisk\n" +
-      "write: 'Hello, world!'.\n"));
+                                             "* Comment asterisk\n" +
+                                             "write: 'Hello, world!'.\n"));
 
     files.push(new File("zcl_main.clas.abap", "class zcl_main defintion.\n" +
-      "endclass.\n" +
-      "class zcl_main implementation.\n" +
-      "endclass.\n"));
+                                              "endclass.\n" +
+                                              "class zcl_main implementation.\n" +
+                                              "endclass.\n"));
 
     const exp = "REPORT zmain13.\n" +
       "* Comment asterisk\n" +
@@ -243,12 +243,12 @@ describe("test 16, REPORT with LINE-SIZE", () => {
   it("something", () => {
     const files = new FileList();
     files.push(new File("zmain14.prog.abap", "REPORT zmain14 LINE-SIZE 100.\n" +
-      "\n" +
-      "write: 'Hello, world!'.\n"));
+                                             "\n" +
+                                             "write: 'Hello, world!'.\n"));
     files.push(new File("zcl_main.clas.abap", "class zcl_main defintion.\n" +
-      "endclass.\n" +
-      "class zcl_main implementation.\n" +
-      "endclass.\n"));
+                                              "endclass.\n" +
+                                              "class zcl_main implementation.\n" +
+                                              "endclass.\n"));
 
     const exp = "REPORT zmain14 LINE-SIZE 100.\n" +
       "\n" +
@@ -270,16 +270,16 @@ describe("test 17, included classes are placed after whitespace and comments", (
   it("something", () => {
     const files = new FileList();
     files.push(new File("zmain17.prog.abap", "REPORT zmain17 LINE-SIZE 100.\n" +
-      "\n" +
-      "* Comment asterisk\n" +
-      "* Epic success!\n" +
-      "\n" +
-      "write: 'Hello, world!'.\n"));
+                                             "\n" +
+                                             "* Comment asterisk\n" +
+                                             "* Epic success!\n" +
+                                             "\n" +
+                                             "write: 'Hello, world!'.\n"));
 
     files.push(new File("zcl_main.clas.abap", "class zcl_main defintion.\n" +
-      "endclass.\n" +
-      "class zcl_main implementation.\n" +
-      "endclass.\n"));
+                                              "endclass.\n" +
+                                              "class zcl_main implementation.\n" +
+                                              "endclass.\n"));
 
     const exp = "REPORT zmain17 LINE-SIZE 100.\n" +
       "\n" +
@@ -305,7 +305,7 @@ describe("test 18, @@abapmerge w/ main no failure", () => {
     const files = new FileList();
     files.push(new File("zmain.abap", "REPORT zmain.\n\nINCLUDE zinc1."));
     files.push(new File("zmain2.prog.abap", "\" @@abapmerge main void\n" +
-      "REPORT zmain2.\n\nINCLUDE zinc1."));
+                                            "REPORT zmain2.\n\nINCLUDE zinc1."));
     files.push(new File("zinc1.abap", "write / 'foo'."));
 
     const result = Merge.merge(files, "zmain");
@@ -373,7 +373,7 @@ describe("test 23, replace report clause name by options", () => {
   it("something", () => {
     const files = new FileList();
     files.push(new File("zmain.abap", "REPORT zmain."));
-    const result = Merge.merge(files, "zmain", { newReportName: "zmain_test" });
+    const result = Merge.merge(files, "zmain", {newReportName: "zmain_test"});
     const split = result.split("\n");
     expect(result).to.be.a("string");
     expect(split.indexOf("REPORT zmain_test.")).to.equal(0);
@@ -384,7 +384,7 @@ describe("test 24, replace report clause name by options with LINE-SIZE", () => 
   it("something", () => {
     const files = new FileList();
     files.push(new File("zmain.abap", "REPORT zmain LINE-SIZE 100."));
-    const result = Merge.merge(files, "zmain", { newReportName: "zmain_test" });
+    const result = Merge.merge(files, "zmain", {newReportName: "zmain_test"});
     const split = result.split("\n");
     expect(result).to.be.a("string");
     expect(split.indexOf("REPORT zmain_test LINE-SIZE 100.")).to.equal(0);
@@ -395,7 +395,7 @@ describe("test 25, replace report clause name by options when report name has na
   it("something", () => {
     const files = new FileList();
     files.push(new File("#prod#main.abap", "REPORT /prod/main."));
-    const result = Merge.merge(files, "#prod#main", { newReportName: "/prod/main_test" });
+    const result = Merge.merge(files, "#prod#main", {newReportName: "/prod/main_test"});
     const split = result.split("\n");
     expect(result).to.be.a("string");
     expect(split.indexOf("REPORT /prod/main_test.")).to.equal(0);
